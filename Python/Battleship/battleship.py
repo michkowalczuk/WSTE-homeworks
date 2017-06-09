cfeea4e554be84f8cd9924f2ba41188dd668a6f6
Michal Kowalczuk <michkowalczuk@gmail.com>
2017-06-10 00:43:10 +0200

    players input done

--- Python/Battleship/battleship.py
+++ Python/Battleship/battleship.py
@@ -16,6 +16,8 @@ BLACK = Color('black')
 GREY = Color('grey')
 RED = Color('red')
 WHITE = Color('white')
+BROWN = Color('brown')
+ORANGE = Color('orange')
 
 
 FONT_NAME = pygame.font.match_font('arial')
@@ -23,7 +25,7 @@ FONT_SIZE = 16
 
 WIDTH = 1000
 HEIGHT = 600
-FPS = 5
+FPS = 30
 TILE_SIZE = 40
 MARGIN_X = 40
 MARGIN_Y = 40
@@ -48,26 +50,36 @@ class TileType(Enum):
     INPUT = -1
 
 class Tile(pygame.sprite.Sprite):
+    player = None
     def __init__(self, index, position, *groups):
         pygame.sprite.Sprite.__init__(self, *groups)  # must be!
-        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image = pygame.Surface((TILE_SIZE-1, TILE_SIZE-1))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.rect.left = position[0]
         self.rect.top = position[1]
         self.index = index
-
+        # self.player = player
         self.isMouseover = False
-        self.type = TileType.NONE
+        # self.type = TileType.NONE
 
-
+    # @property
+    # def i(self):
+    #     return self.index[0]
+    #
+    # @property
+    # def j(self):
+    #     return self.index[1]
 
     def update(self):
+        # print(Tile.player.board)
         if self.isMouseover:
             self.image.fill(RED)
         else:
-            self.image.fill(WHITE)
-
+            if Tile.player.board[self.index[1], self.index[0]] in range(1,5):
+                self.image.fill(BLUE)
+            else:
+                self.image.fill(WHITE)
 
 
 class Player(object):
@@ -75,13 +87,15 @@ class Player(object):
         self.board = np.zeros((10,10))
 
     def mast_count(self, n):
-        return np.count_nonzero(self.board == n)
+        return int(np.count_nonzero(self.board == n) / n)
 
     @property
     def fleet_complete(self):
         # return (self.mast_count(5) == 0 )
-        return any([(self.mast_count(i) == 5 - i) for i in range(1,5)])
+        return all([(self.mast_count(i) == 5 - i) for i in range(1,5)])
 
+    def setShip(self, n, index):
+        self.board[index[1], index[0]] = n
 
 
 def draw_text(surf, text, size, x, y, align=TextAlign.CENTER):
@@ -117,12 +131,12 @@ def draw_tile(surf, x, y):
     pygame.draw.rect(surf, BLUE, tile_rect, 1)
 
 
-# def draw_horizontal_line(surf, x):
-#     pygame.draw.line(surf, BLUE, (x, TILE_Y[0]), (x, TILE_Y[-1] + TILE_SIZE))
-#
-#
-# def draw_vertical_line(surf, x):
-#     pygame.draw.line(surf, BLUE, (x, TILE_Y[0]),(x, TILE_Y[-1]))
+def draw_horizontal_line(surf, x):
+    pygame.draw.line(surf, BLUE, (x, TILE_Y[0]), (x, TILE_Y[-1] + TILE_SIZE))
+
+
+def draw_vertical_line(surf, x):
+    pygame.draw.line(surf, BLUE, (x, TILE_Y[0]),(x, TILE_Y[-1]))
 
 
 # INITIALIZE PYGAME AND CREATE WINDOW
@@ -140,14 +154,16 @@ for i, x in enumerate(TILE_X):
     for j, y in enumerate(TILE_Y):
         tile = Tile((i,j), (x, y), all_sprites)
         all_sprites.add(tile)
-player = Player()
 
+player = Player()
+Tile.player = player
 
 # GAME LOOP
 # instructions = True
 running = True
 x,y = 0, 0
 mouseClicked = False
+clickedTileIndex = []
 while running:
 
     # keep loop running at the right speed
@@ -169,28 +185,27 @@ while running:
                 mouseClicked = True
 
 
-
     # UPDATE
     all_sprites.update()
 
     for tile in all_sprites:
         if tile.rect.collidepoint(x,y):
-            print((x,y))
+            # print((x,y))
             tile.isMouseover = True
-            # pygame.draw.rect(screen, RED, tile.rect, 2)
-            # tile.image.fill(RED)
-            # print("ttt")
+            if mouseClicked:
+                clickedTileIndex = tile.index
+            #     player.SetShip(i, tile.index)
+            #     print(player.board)
         else:
             tile.isMouseover = False
             # print("nnn")
-
-
 
 
     # DRAW / RENDER
     screen.fill(GREY)
     all_sprites.draw(screen)
 
+    # draw_board_lines(screen, )
 
     # draw_board(screen)  # player's board
     # draw_board(screen, 500)  # computer's board
@@ -207,11 +222,16 @@ while running:
     #         tile.isMouseover = False
     #         # print("nnn")
 
+    # input fleet part
     if not player.fleet_complete:
         instructions =["Wprowadz:"]
         for i in range(1,5):
-            instructions.append("{} x {}-maszt".format((5 - i) - player.mast_count(5 - i), i))
-
+            if player.mast_count(i) != 5 - i:
+                instructions.append("{} x {}-maszt".format((5 - i) - player.mast_count(i), i))
+                if mouseClicked:
+                    player.setShip(i, clickedTileIndex)
+                    # print(player.board)
+                break
         for i, instruction in enumerate(instructions):
             x_instruction = MARGIN_X
             y_instruction = TILE_Y[-1] + TILE_SIZE + MARGIN_Y/2 + i * TILE_SIZE/2
